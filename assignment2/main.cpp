c97e943c81a7c280eb0af931ca383bb11779b4bd
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2022-04-30 21:29:07 +0300

    Latest stuff (task 3)

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -10,6 +10,7 @@
 const uint8_t FIRST_BIT = (1 << 7);
 const uint8_t FIRST_BIT_OFF = FIRST_BIT - 1;
 const uint64_t MODULUS_128 = (((1UL << 63) - 1) >> (63 - 7));
+const uint64_t MODULUS_64 = (((1UL << 63) - 1) >> (63 - 6));
 
 void print_time(std::chrono::_V2::system_clock::time_point start, std::chrono::_V2::system_clock::time_point end) {
     std::cerr << std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count() << std::endl;
@@ -17,82 +18,159 @@ void print_time(std::chrono::_V2::system_clock::time_point start, std::chrono::_
 
 class BitArray {
     private:
-        int k = 8;
-
-        std::vector<uint8_t> bv;
+        uint64_t k = 0;
+        uint64_t acual_block_size = 0;
+        uint64_t first_bit = 0;
+        uint64_t first_bit_off = 0;
+        uint64_t modulus = 0;
+        uint64_t last_block_pos = 0;
+        std::vector<uint64_t> bv;
+        uint64_t n_blocks = 0;
     
     public:
+
+        BitArray(uint64_t __k) {
+            k = __k;
+            acual_block_size = k + 1;
+            modulus = (((1UL << 63) - 1) >> (63 - k));
+            first_bit = (1UL << k);
+            first_bit_off = first_bit - 1;
+            bv.push_back(0UL);
+        }
 
         void add(uint64_t num) {
             uint64_t block;
 
             while (true) {
-                block = num & MODULUS_128;
+                n_blocks++;
+                // what to add next
+                block = num & modulus;
+                // if less than 1 >> k, set stop bit
+                if (num < first_bit) {
+                    block += first_bit;
+                    // std::cerr << "stop bit " << block-first_bit << " ";
+                }
 
-                if (num < 128) {
-                    bv.push_back(block + 128UL); 
+
+                // std::cerr << last_block_pos << " ";
+
+                uint64_t space_left = 64 - last_block_pos;
+                if (space_left == 0) {
+                    bv.push_back(block);
+                    last_block_pos = acual_block_size;
+                    // std::cout << "B " << " ";
+                } else {
+                    bv[bv.size() - 1] |= (block << last_block_pos);
+                    last_block_pos += acual_block_size;
+
+                    if (space_left < acual_block_size) {
+                        uint64_t remaining = acual_block_size - space_left;
+                        uint64_t new_block = (block >> space_left) & ((1UL << remaining) - 1);
+                        bv.push_back(new_block);
+                        last_block_pos = remaining;
+                        // std::cout << "Br " << block << " " << space_left << " " << remaining << " " << new_block << "\n";
+                    }
+                    
+                }
+                
+                // std::cerr << bv[bv.size() - 1] << "\n";
+                if (num < first_bit) {
                     break;
                 }
-                bv.push_back(block);
-                num >>= 7;
+                
+
+                num >>= k;
             }
         }
 
         void decode() {
-            int i = 0;
-            int j = 0;
+            // std::cerr << (bv[0] & first_bit_off) << "\n";
+            uint64_t i = 0;
             uint64_t sum = 0UL;
 
             uint64_t block;
-            while (i < bv.size()) {
-                block = bv[i];
-                bool stop = block & FIRST_BIT;
-                block &= FIRST_BIT_OFF;
+            uint64_t bv_block;
+            uint64_t bv_block_internal_index = 0;
+            uint64_t bv_current_block_index = 0;
+            
+            while (i < n_blocks) {
+                uint64_t bv_index = (i * acual_block_size) / 64;
+                bv_block = bv[bv_index];
+                block = bv_block >> bv_block_internal_index;
 
-                sum |= block << j;
+                uint64_t space_left = 64 - bv_block_internal_index;
+                if (space_left < acual_block_size) {
+                    uint64_t remaining = acual_block_size - space_left;
+                    uint64_t remaining_bits = (bv[bv_index + 1] & ((1UL << remaining) - 1)) << (acual_block_size - remaining);
+                    block |= remaining_bits;
+                    // std::cerr << "While decoding, one block spanned multiple bv_blocks " << remaining << " " << remaining_bits << " " << block << " " << (bv[bv_index + 1] & ((1UL << remaining) - 1)) << "\n";
+                }
 
-                j += 7;
+                bool stop = block & first_bit;
+                block &= first_bit_off;
+
+                // std::cout << bv_index << " " << bv_block_internal_index << " " << bv_current_block_index << " " << bv_block << " " << block << " " << stop << "\n";
+
+                sum |= block << bv_current_block_index;
+
+                bv_block_internal_index = (bv_block_internal_index + acual_block_size) & MODULUS_64;
+                bv_current_block_index += k;
                 i++;
 
                 if (stop) {
                     std::cout << sum << "\n";
                     sum = 0UL;
-                    j = 0;
+                    bv_current_block_index = 0;
                 }
             }
             
         }
 
         void decode_s() {
-            int i = 0;
-            int j = 0;
+            // std::cerr << (bv[0] & first_bit_off) << "\n";
+            uint64_t i = 0;
             uint64_t sum = 0UL;
-            uint64_t prev = 0UL;
 
             uint64_t block;
-            while (i < bv.size()) {
-                block = 0UL;
-                block = bv[i];
-                bool stop = block & FIRST_BIT;
-                block &= FIRST_BIT_OFF;
+            uint64_t bv_block;
+            uint64_t bv_block_internal_index = 0;
+            uint64_t bv_current_block_index = 0;
+            uint64_t prev = 0;
+            while (i < n_blocks) {
+                uint64_t bv_index = (i * acual_block_size) / 64;
+                bv_block = bv[bv_index];
+                block = bv_block >> bv_block_internal_index;
 
-                sum |= block << j;
+                uint64_t space_left = 64 - bv_block_internal_index;
+                if (space_left < acual_block_size) {
+                    uint64_t remaining = acual_block_size - space_left;
+                    uint64_t remaining_bits = (bv[bv_index + 1] & ((1UL << remaining) - 1)) << (acual_block_size - remaining);
+                    block |= remaining_bits;
+                    // std::cerr << "While decoding, one block spanned multiple bv_blocks " << remaining << " " << remaining_bits << " " << block << " " << (bv[bv_index + 1] & ((1UL << remaining) - 1)) << "\n";
+                }
 
-                j += 7;
+                bool stop = block & first_bit;
+                block &= first_bit_off;
+
+                // std::cout << bv_index << " " << bv_block_internal_index << " " << bv_current_block_index << " " << bv_block << " " << block << " " << stop << "\n";
+
+                sum |= block << bv_current_block_index;
+
+                bv_block_internal_index = (bv_block_internal_index + acual_block_size) & MODULUS_64;
+                bv_current_block_index += k;
                 i++;
 
                 if (stop) {
-                    sum += prev;
-                    std::cout << sum << "\n";
-                    prev = sum;
+                    std::cout << sum+prev << "\n";
+                    prev = sum+prev;
                     sum = 0UL;
-                    j = 0;
+                    bv_current_block_index = 0;
                 }
             }
         }
 
         std::size_t size() {
-            return bv.size();
+            return n_blocks;
         }
 };
 
@@ -114,21 +192,22 @@ int main(int argc, char const* argv[]) {
     uint64_t s;
     std::fread((char*)&s, sizeof(uint64_t), 1, stdin);
 
+    // k = k < 2 ? 7 : k;
+    // k = 7;
     std::cerr << n << " " << k << " " << s << std::endl;
 
-    BitArray b;
+    BitArray b(k);
 
     std::chrono::high_resolution_clock clock;
     auto start = clock.now();
 
-    int i = 0;
+    uint64_t i = 0;
     uint64_t prev = 0UL;
 
     const size_t BUFFER_SIZE = 64;
     uint64_t num;
     uint64_t buffer[BUFFER_SIZE];
-    int mod = 0;
-    
+    uint64_t mod = 0;
     if (s > 0) {
         while (i < n) {
             mod = i % BUFFER_SIZE;
@@ -159,7 +238,9 @@ int main(int argc, char const* argv[]) {
 
 
     std::cout << b.size() << "\n";
-    std::cerr << b.size() << "\n";
+    u_int64_t size = b.size() * k;
+    double compression = size / (1.0 * n * 64);
+    std::cerr << "Blocks: " << b.size() << " Compression ratio: " << compression << "\n";
     
     start = clock.now();
 
